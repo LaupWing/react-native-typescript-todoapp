9b246267334d15c7279ba6b7198f76cffcdb5362
LaupWing <loc_strict@hotmail.com>
2023-02-08 10:31:28 +0100

    alllright

--- App.tsx
+++ App.tsx
@@ -1,7 +1,9 @@
 import { GoogleSignin } from "@react-native-google-signin/google-signin"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
+import { useEffect } from "react"
 import { StyleSheet, Text, View } from "react-native"
+import auth from "@react-native-firebase/auth"
 import Home from "./screens/Home"
 import Login from "./screens/Login"
 
@@ -12,10 +14,20 @@ export type RootStackParamsList = {
 
 const Stack = createNativeStackNavigator<RootStackParamsList>()
 
+GoogleSignin.configure({
+   webClientId: "1088828136827-qu7hd60qceh11p586okglsam3g62ess1.apps.googleusercontent.com"
+})
+
 export default function App() {
-   // GoogleSignin.configure({
-   //    webClientId: "1088828136827-qu7hd60qceh11p586okglsam3g62ess1.apps.googleusercontent.com"
-   // })
+   const onAuthStateChanged = (user:any) => {
+      console.log(user)
+   }
+
+   useEffect(() => {
+      const subscriber = auth().onAuthStateChanged(onAuthStateChanged)
+      return subscriber
+   }, [])
+
    return (
       <NavigationContainer>
          <Stack.Navigator>
