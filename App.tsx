9d0f1d845402ff65645bda602d6d7fc9c39947ea
LaupWing <loc_strict@hotmail.com>
2023-02-10 11:23:04 +0100

    udpate

--- App.tsx
+++ App.tsx
@@ -16,16 +16,22 @@ const Stack = createNativeStackNavigator<RootStackParamsList>()
 export default function App() {
    return (
       <NavigationContainer>
-         <View className="flex-1 bg-red-400">
-            {/* <LinearGradient className="flex-1" colors={["#6366f1", "#a855f7", "#ec4899"]}>
-               <Text>Open up App.js to start working on your app!</Text>
-               <StatusBar style="auto" />
-            </LinearGradient> */}
-            <Stack.Navigator>
-               <Stack.Screen name="Home" component={Home}/>
-               <Stack.Screen name="Login" component={Login}/>
-            </Stack.Navigator>
-         </View>
+         <Stack.Navigator>
+            <Stack.Group
+               screenOptions={{
+                  headerShown: false
+               }}
+            >
+               <Stack.Screen 
+                  name="Home" 
+                  component={Home}
+               />
+               <Stack.Screen 
+                  name="Login" 
+                  component={Login}
+               />
+            </Stack.Group>
+         </Stack.Navigator>
       </NavigationContainer>
    )
 }
