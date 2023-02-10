4fd3ed5e90ba879fa1e1cf59ced58922f1870753
LaupWing <loc_strict@hotmail.com>
2023-02-10 11:23:26 +0100

    removed unused

--- App.tsx
+++ App.tsx
@@ -1,6 +1,3 @@
-import { StatusBar } from "expo-status-bar"
-import { Text, View } from "react-native"
-import { LinearGradient } from "expo-linear-gradient"
 import { NavigationContainer } from "@react-navigation/native"
 import { createNativeStackNavigator } from "@react-navigation/native-stack"
 import Home from "./screens/Home"
