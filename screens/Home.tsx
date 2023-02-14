9e9d57324c71f944ddd604255eb3ece31d818674
LaupWing <loc_strict@hotmail.com>
2023-02-14 11:13:13 +0100

    wtf

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -4,8 +4,7 @@ import {
    TextInput,
    FlatList,
    TouchableOpacity, 
-   SafeAreaView,
-   ScrollView
+   SafeAreaView
 } from "react-native"
 import { NativeStackNavigationProp } from "@react-navigation/native-stack"
 import { RootStackParamsList } from "../App"
@@ -22,41 +21,34 @@ export type NavigationProp = NativeStackNavigationProp<
 const Home = () => {
    const [todos, setTodos] = useState(_todos)
 
-   {/* <View className="w-full bg-indigo-600 p-2">
-      <Text className="text-white uppercase font-bold text-2xl tracking-wider">todo app</Text>
-   </View>
-   <View className="flex flex-row ">
-      <TextInput
-         placeholder="Add a new todo"
-         className="px-2 py-1 flex-1"
-      />
-      <TouchableOpacity className="bg-indigo-500 px-4 items-center justify-center">
-         <Text className="text-white uppercase font-bold">add</Text>
-      </TouchableOpacity>
-   </View> */}
    return (
-      // <Layout>
-        // <View className="bg-white rounded w-full border-[3px] border-indigo-600">
-         <ScrollView className="flex flex-col">
-            {todos.map(todo => <Todo key={todo.text} item={todo}/>)}  
-         </ScrollView>
-            // <FlatList
-            //    data={todos}
-            //    className=""
-            //    renderItem={({item})=> <Todo item={item}/>}
-            //    keyExtractor={item => item.text}
-            // />
-       //  </View>
-      // </Layout>
+      <Layout>
+         <View className="bg-white mt-[10vh] rounded w-full flex-1 mb-4 border-[3px] border-indigo-600">
+            {/* <View className="w-full bg-indigo-600 p-2">
+               <Text className="text-white uppercase font-bold text-2xl tracking-wider">todo app</Text>
+            </View>
+            <View className="flex flex-row ">
+               <TextInput
+                  placeholder="Add a new todo"
+                  className="px-2 py-1 flex-1"
+               />
+               <TouchableOpacity className="bg-indigo-500 px-4 items-center justify-center">
+                  <Text className="text-white uppercase font-bold">add</Text>
+               </TouchableOpacity>
+            </View> */}
+            <FlatList
+               data={todos}
+               renderItem={({item})=>
+                  <View className="py-2 border-b">
+                     <Text>
+                        {item.text}
+                     </Text>
+                  </View>
+               }
+               keyExtractor={item => item.text}
+            />
+         </View>
+      </Layout>
    )
 }
 export default Home
-
-
-const Todo = ({item}) =>(
-   <View className="py-2 border-b">
-      <Text>
-         {item.text}
-      </Text>
-   </View>
-)
