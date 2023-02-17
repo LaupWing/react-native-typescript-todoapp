e6c517bf35aa694eabf5e4c4c68d1a69bb313d93
LaupWing <loc_strict@hotmail.com>
2023-02-17 10:36:23 +0100

    uninstalled

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -44,12 +44,12 @@ const Home = () => {
                renderItem={({ item }) => (
                   <View className="flex-row items-center border-b border-gray-300 last:border-0">
                      <Text className="flex-1 px-2">{item.text}</Text>
-                     <View className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
+                     <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
                         <MaterialIcons color={"green"} name="edit" size={24}/>
-                     </View>
-                     <View className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
+                     </TouchableOpacity>
+                     <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
                         <FontAwesome color={"red"} name="trash" size={24}/>
-                     </View>
+                     </TouchableOpacity>
                   </View>
                )}
                keyExtractor={(item) => item.text}
