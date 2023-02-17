f0225e17196bb6601291200b8e359fc91ae967e9
LaupWing <loc_strict@hotmail.com>
2023-02-17 11:28:53 +0100

    setUserId

--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -15,12 +15,14 @@ export const userSlice = createSlice({
    name: "user",
    initialState,
    reducers: {
-      
+      setUserId(state, action: PayloadAction<string>){
+         state.id = action.payload
+      }
    },
 })
 
 export const { 
-   
+   setUserId
 } = userSlice.actions
 
 
