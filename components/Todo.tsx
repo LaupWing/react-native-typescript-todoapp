99ff43751fa06682ea9aec329a4b86097610ac48
LaupWing <loc_strict@hotmail.com>
2023-02-17 10:51:53 +0100

    udpate

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,15 +1,20 @@
-import { View, Text } from "react-native"
+import { FC } from "react"
+import { View, Text, TouchableOpacity } from "react-native"
+import { FontAwesome, MaterialIcons } from "@expo/vector-icons"
+import { TodoType } from "../types"
 
-const Todo = () => {
+const Todo:FC<{item: TodoType}> = ({ 
+   item
+}) => {
    return (
       <View className="flex-row items-center border-b border-gray-300 last:border-0">
-         {/* <Text className="flex-1 px-2">{item.text}</Text>
+         <Text className="flex-1 px-2">{item.text}</Text>
          <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
             <MaterialIcons color={"green"} name="edit" size={24}/>
          </TouchableOpacity>
          <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
             <FontAwesome color={"red"} name="trash" size={24}/>
-         </TouchableOpacity> */}
+         </TouchableOpacity>
       </View>
    )
 }
