032fbdcc4df2ddecb307aa6022f458fd76a80d06
LaupWing <loc_strict@hotmail.com>
2023-02-17 10:03:21 +0100

    hm

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -7,17 +7,21 @@ const Layout:FC<PropsWithChildren> = ({ children }) => {
    const insets = initialWindowMetrics.insets
 
    return (
-      <LinearGradient 
-         className="justify-center items-center flex flex-1" 
-         colors={["#6366f1", "#a855f7", "#ec4899"]}
+      <View
+         className="flex-1"
          style={{
             paddingBottom: insets.bottom
          }}
       >
-         <SafeAreaView className="flex-1 w-full px-6">
-            {children}
-         </SafeAreaView>
-      </LinearGradient>
+         <LinearGradient 
+            className="justify-center items-center flex flex-1" 
+            colors={["#6366f1", "#a855f7", "#ec4899"]}
+         >
+            <View className="px-6 flex-1 w-full">
+               {children}
+            </View>
+         </LinearGradient>
+      </View>
    )
 }
 export default Layout
