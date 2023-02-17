a02364d5eb31fe103e58c748fa4754e3946cdd86
LaupWing <loc_strict@hotmail.com>
2023-02-17 10:38:07 +0100

    Todo cmp

---
+++ components/Todo.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native"
+
+const Todo = () => {
+   return (
+      <View className="flex-row items-center border-b border-gray-300 last:border-0">
+         {/* <Text className="flex-1 px-2">{item.text}</Text>
+         <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
+            <MaterialIcons color={"green"} name="edit" size={24}/>
+         </TouchableOpacity>
+         <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
+            <FontAwesome color={"red"} name="trash" size={24}/>
+         </TouchableOpacity> */}
+      </View>
+   )
+}
+export default Todo
