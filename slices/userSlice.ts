24533372506483251242debd8cb9a620e81c8757
LaupWing <loc_strict@hotmail.com>
2023-02-20 10:29:46 +0100

    updated

--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -1,11 +1,16 @@
-import { createSlice, PayloadAction } from "@reduxjs/toolkit"
+import type { TodoType } from "../types"
+import { createSlice, Dispatch, PayloadAction } from "@reduxjs/toolkit"
+import { store } from "../redux/store"
+import firestore from "@react-native-firebase/firestore"
 
 interface UserState {
    id: string
+   todos: TodoType[]
 }
 
 const initialState: UserState = {
    id: undefined,
+   todos: []
 }
 
 export const userSlice = createSlice({
@@ -17,6 +22,24 @@ export const userSlice = createSlice({
       }
    },
 })
+
+export const getTodos = 
+   () => async () => {
+      
+   }
+
+export const addTodo = 
+   (todo: TodoType) => async (dipatch: Dispatch, getState: typeof store.getState) => {
+      const { id } = getState().user
+      const newTodo = await firestore()
+         .collection("users")
+         .doc(id)
+         .collection("todos")
+         .doc()
+         .set(todo)
+
+      console.log(newTodo)
+   }
 
 export const { 
    setUserId
