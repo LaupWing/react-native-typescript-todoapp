ca1a56870a18cf78ef40c04b183a18cea215b26b
LaupWing <loc_strict@hotmail.com>
2023-02-20 10:02:57 +0100

    removed unused

--- slices/userSlice.ts
+++ slices/userSlice.ts
@@ -1,12 +1,9 @@
 import { createSlice, PayloadAction } from "@reduxjs/toolkit"
-import { RootState } from "../redux/store"
 
-// Define a type for the slice state
 interface UserState {
    id: string
 }
 
-// Define the initial state using that type
 const initialState: UserState = {
    id: undefined,
 }
