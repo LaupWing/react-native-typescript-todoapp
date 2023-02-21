598d461a1cf70d47cf86e9a206837bef07a92671
LaupWing <loc_strict@hotmail.com>
2023-02-21 10:17:45 +0100

    clena up

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -8,7 +8,6 @@ import { deleteTodo } from "../slices/userSlice"
 const Todo:FC<{item: TodoType}> = ({ 
    item
 }) => {
-   const dispatch = useAppDispatch()
    const [edit, setEdit] = useState(false)
 
    return (
@@ -19,25 +18,25 @@ const Todo:FC<{item: TodoType}> = ({
             ) : (
                <Text className="flex-1 px-2">{item.text}</Text>
          )}
-         <Buttons id={item.id}/>
+         <Buttons id={item.id} setEdit={setEdit}/>
       </View>
    )
 }
 export default Todo
 
 
-const Buttons = ({id}) => {
+const Buttons = ({id, setEdit}) => {
    const dispatch = useAppDispatch()
    const onPressDelete = () => {
       dispatch(deleteTodo(id))
    }
    return (
-      <View 
-         className="flex-row transform translate-x-full"
-         
-      >
+      <View className="flex-row transform translate-x-full">
          <View className="flex-row translate-x-20">
-            <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
+            <TouchableOpacity 
+               className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square"
+               onPress={() => setEdit(true)}
+            >
                <MaterialIcons color={"green"} name="edit" size={24}/>
             </TouchableOpacity>
             <TouchableOpacity 
@@ -47,7 +46,7 @@ const Buttons = ({id}) => {
                <FontAwesome color={"red"} name="trash" size={24}/>
             </TouchableOpacity>
          </View>
-         <View className="flex-row">
+         <View className="flex-row translate-x-20">
             <TouchableOpacity className="border-l border-gray-300 w-10 flex items-center justify-center aspect-square">
                <FontAwesome color={"green"} name="check" size={24}/>
             </TouchableOpacity>
